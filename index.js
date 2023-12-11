5905c79fc9040fd10ff96c599aaff5266c4b212b
Walisson Araujo <walisson.araujo@ctec.ufal.br>
2023-12-11 13:46:58 -0300

    Implementação de 3 funcionalidades novas

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const imageList = ['images/background-image-0.jpg',
-                            'images/background-image-1.jpg',
-                            'images/background-image-2.jpg',
-                            'images/background-image-3.jpg'];
+// BACKGROUND CAROUSEL IMPLEMENTATION
+
+const imageList = ['images/background-image-0.jpg', 'images/background-image-1.jpg', 'images/background-image-2.jpg', 'images/background-image-3.jpg'];
 
 let slideIndex = 0;
 let backgroundImage = document.getElementById("backgroundImage");
@@ -11,7 +10,8 @@ window.setInterval(changeImageByTime, 3000);
 document.getElementById("leftSlide").addEventListener("click", previousSlide);
 document.getElementById("rightSlide").addEventListener("click", nextSlide);
 
-console.log(-1%4);
+console.log(imageList[-1]);
+console.log(-1 % 5);
 
 function changeImageByTime() {
     if (slideIndex >= imageList.length) {
@@ -45,3 +45,68 @@ function previousSlide() {
     changeImage()
 }
 
+// SLIDE MENU IMPLEMENTATION
+
+let slideButton = document.getElementById("slideButton");
+let slideMenu = document.getElementById("slideMenu");
+let body = document.body;
+
+slideButton.addEventListener("click", function () {
+    slideMenu.style.display = "block";
+    body.style.overflow = "hidden";
+    body.style.maxHeight = "100%";
+});
+
+let closeMenuButton = document.getElementById("closeButton");
+closeMenuButton.addEventListener("click", function () {
+    slideMenu.style.display = "none";
+    body.style.overflow = "auto";
+    body.style.maxHeight = "none";
+});
+
+// DROP DOWN MENU IMPLEMENTATION
+
+let dropDownButton = document.getElementById("dropDownButton");
+let dropDownMenu = document.getElementById("dropDownMenu");
+
+dropDownButton.addEventListener("click", function () {
+    if (dropDownMenu.style.display === "none") {
+        dropDownButton.style.outline = "2px solid orange";
+        dropDownMenu.style.display = "block";
+    } else {
+        dropDownMenu.style.display = "none";
+    }
+});
+
+const dropDownItems = document.querySelectorAll('.drop-down li');
+let searchBar = document.getElementById('searchBar');
+let transparentFrame = document.getElementById('transparentFrame');
+let inputText = document.getElementById("inputText");
+
+dropDownItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+        dropDownButton.textContent = item.textContent.trim();
+        searchBar.style.outline = "2px solid orange";
+        transparentFrame.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        inputText.focus();
+        dropDownMenu.style.display = "none";
+        transparentFrame.style.zIndex = "1000";
+
+    });
+});
+
+// CLOSE DROP DOWN MENU WHEN CLICKING OUTSIDE OF IT
+document.addEventListener("click", function (event) {
+    if (event.target === transparentFrame) {
+        dropDownMenu.style.display = "none";
+        transparentFrame.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        transparentFrame.style.zIndex = "-1";
+    }
+});
+
+// CLOSE SLIDE MENU WHEN CLICKING OUTSIDE OF IT
+document.addEventListener("click", function (event) {
+    if (event.target === slideMenu) {
+        slideMenu.style.display = "none";
+    }
+});
